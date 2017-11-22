394c1bfead0060e89300d71ab70297b539603621
Unknown <wod.moshkin@gmail.com>
2017-11-22 13:15:38 +0200

    New

---
+++ TelerikExam13October/Problem01CrookedDigits/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Problem04
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            long[] wallHeight = Console.ReadLine().Split(new char[] { ' ' },
+                                                     StringSplitOptions
+                                                     .RemoveEmptyEntries)
+                                       .Select(long.Parse).ToArray();
+
+
+            List<long> wallDiff = new List<long>();
+
+
+            for (int i = 1; i < wallHeight.Length; i++)
+            {
+                long wallABS = Math.Abs(wallHeight[i] - wallHeight[i - 1]);
+                if (wallDiff.Count < 1 || wallDiff[i - 2] == 0)
+                {
+                    wallDiff.Add(wallABS);
+
+
+                }
+                else
+                {
+                    if (wallDiff[i - 2] % 2 == 0)
+                    {
+                        wallDiff.Add(0);
+                    }
+                    else
+                    {
+                        wallDiff.Add(wallABS);
+                    }
+                }
+
+
+            }
+            long sum = 0;
+            for (int i = 0; i < wallDiff.Count; i++)
+            {
+                if (wallDiff[i] % 2 == 0)
+                {
+                    sum += wallDiff[i];
+                }
+            }
+
+
+            //Console.WriteLine(string.Join(" ", wallDiff));
+            Console.WriteLine(string.Join(" ", sum));
+
+        }
+    }
+}
